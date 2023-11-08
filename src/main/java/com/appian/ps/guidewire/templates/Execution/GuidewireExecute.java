e1b01b87eb98181fc884d2f7b1e6cb0ce25d45bd
Julian Grunauer <julian.grunauer@appian.com>
2023-11-08 10:34:04 -0500

    AppMarket changes and added composite

--- src/main/java/com/appian/ps/guidewire/templates/Execution/GuidewireExecute.java
+++ src/main/java/com/appian/ps/guidewire/templates/Execution/GuidewireExecute.java
@@ -162,9 +162,6 @@ public class GuidewireExecute extends Execute {
 
       String compositeProp = integrationConfiguration.getValue("composite");
       return RequestBody.create(compositeProp, MediaType.get("application/json; charset=utf-8"));
-/*      Map<String, Object> compositeReqBody = objectMapper.readValue(compositeProp, Map.class);*/
-
-
     }
 
     // If not request body is needed for the post request
@@ -188,17 +185,15 @@ public class GuidewireExecute extends Execute {
     if (doc != null) {
 
       // Create a temporary file and copy the InputStream into it
-      File tempFile = null;
       String fileName = doc.getFileName();
       String fileNameWithoutExtension = fileName.substring(0, doc.getFileName().lastIndexOf("."));
+      File tempFile = null;
+      FileOutputStream out = null;
       try {
         tempFile = File.createTempFile(fileNameWithoutExtension, "." + doc.getExtension());
-        FileOutputStream out = new FileOutputStream(tempFile);
+        out = new FileOutputStream(tempFile);
         IOUtils.copy(doc.getInputStream(), out);
       } finally {
-        if (tempFile != null) {
-          tempFile.deleteOnExit();
-        }
         // Create MultipartBody
         String jsonString = new ObjectMapper().writeValueAsString(dataWrapper);
         // Determine media type based on file extension
@@ -207,6 +202,12 @@ public class GuidewireExecute extends Execute {
           contentType = "application/octet-stream";  // Default to octet stream if type is unknown
         }
         RequestBody fileBody = RequestBody.create(tempFile, MediaType.parse(contentType));
+        if (out != null) {
+          out.close();
+        }
+        if (tempFile != null) {
+          tempFile.deleteOnExit();
+        }
         return new MultipartBody.Builder()
             .setType(MultipartBody.FORM)
             .addFormDataPart("content", fileName, fileBody)
